42b83bc636c56d98852a1148e5a5765d3f94cbf6
aws108 <sergiolizanagarcia@gmail.com>
2024-05-03 16:01:35 +0200

    crear standalone components y pasar un componente normal a standalone

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
 
 @Component({
-  // selector: 'app-alone-page',
-  standalone: true,
-  imports: [CommonModule, CounterAloneComponent], //Standalone que contiene otro standalone
-  templateUrl: './alone-page.component.html',
-  styleUrls: ['./alone-page.component.css']
+    // selector: 'app-alone-page',
+    standalone: true, //Standalone que contiene otro standalone
+    templateUrl: './alone-page.component.html',
+    styleUrls: ['./alone-page.component.css'],
+    imports: [CommonModule, CounterAloneComponent, SideMenuComponent]
 })
 export class AlonePageComponent {
 
